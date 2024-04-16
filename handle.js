0cc9a8e56f406de35d6b768fb51bfe199c163dd5
premdeep909 <premdeep909@gmail.com>
2024-04-16 10:53:11 +0530

    Routing Request

--- handle.js
+++ handle.js
@@ -1,7 +1,14 @@
 // how to create server 
 const http = require('http');
 const server = http.createServer((req,res) =>{
-//console.log(req);
+const url =  req.url;
+if(url === '/'){
+    res.write('<html>');
+    res.write('<head><title>Node JS response</title></head>');
+    res.write('<body><from action="/message" method="POST"><input type="text" name="myMessage"/><button type="submit" >Submit</button></from></body>');
+    res.write('</html>');
+    return res.end();
+}
 res.setHeader('Content-Type' ,'text/html');
 res.write('<html>')
 res.write('<header><title>Node JS response</title></header>')
