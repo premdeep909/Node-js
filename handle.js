6e93ac6bbf4b75ba7aa434f69201a536a57c70d2
premdeep909 <premdeep909@gmail.com>
2024-04-16 09:23:30 +0530

    Sending Response To Frontend

--- handle.js
+++ handle.js
@@ -1,6 +1,12 @@
 // how to create server 
 const http = require('http');
 const server = http.createServer((req,res) =>{
-console.log(req);
+//console.log(req);
+res.setHeader('Content-Type' ,'text/html');
+res.write('<html>')
+res.write('<header><title>Node JS response</title></header>')
+res.write('<body><h1>Hello From Node JS!</h1></body>')
+res.write('</html>')
+res.end();
 })
 server.listen(3000);
