f77cf0b73be9c35b1912a83eb4ec3a69ee9ebd92
premdeep909 <premdeep909@gmail.com>
2024-04-16 11:47:52 +0530

    Redirecting Requests

--- handle.js
+++ handle.js
@@ -1,12 +1,20 @@
 // how to create server 
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req,res) =>{
 const url =  req.url;
+const method = req.method;
 if(url === '/'){
     res.write('<html>');
     res.write('<head><title>Node JS response</title></head>');
     res.write('<body><form action="/message" method="post"><input type="text" name="myMessage"/><button type="submit" >Submit</button></form></body>');
     res.write('</html>');
+    return res.end();
+}
+if(url === '/message' && method === 'POST'){
+    fs.writeFileSync('myMsg.text','my test data');
+    res.statusCode = 302;
+    res.setHeader('Location','/');
     return res.end();
 }
 res.setHeader('Content-Type' ,'text/html');
